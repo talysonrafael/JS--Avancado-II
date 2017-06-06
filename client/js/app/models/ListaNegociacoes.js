2e0b30654ad2be619754a8a8c88b3571f8bbc917
Talyson Leocádio <talyson.rafael.leocadio@gmail.com>
2017-06-06 09:17:41 -0300

    Criação de função para exclusão de negociações

--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,17 +1,21 @@
 class ListaNegociacoes {
     
-    constructor() {
-        
+    constructor(armadilha) {
         this._negociacoes = [];
+        this._armadilha = armadilha;
     }
     
     adiciona(negociacao) {
-        
         this._negociacoes.push(negociacao);
+        this._armadilha(this);
     }
     
     get negociacoes() {
-        
         return [].concat(this._negociacoes);
+    }
+
+    apaga() {
+        this._negociacoes = [];
+        this._armadilha(this);
     }
 }
